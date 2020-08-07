29aab1a3cc80f010f19382c6739ac8c32ef82538
anasbajjouk <a.bajjouk@cleo.one>
2020-08-07 19:05:05 +0200

    Updates

---
+++ precache-manifest.b47746df5c5257236cace3f7a7a37f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677cfd6b91bc48304fd36e080ce16d68",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "3f4a5e2a4db0dc751b4e",
+    "url": "/hangman/static/css/main.5be84a40.chunk.css"
+  },
+  {
+    "revision": "0f019097610fba32d94b",
+    "url": "/hangman/static/js/2.907532aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.907532aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4a5e2a4db0dc751b4e",
+    "url": "/hangman/static/js/main.8e3310a3.chunk.js"
+  },
+  {
+    "revision": "dda74474cccc8c6444c0",
+    "url": "/hangman/static/js/runtime-main.a131f507.js"
+  }
+]);
